25d0320be23b2ff2e15ccd7c973fb5d893366813
David Golden <dagolden@cpan.org>
2011-08-22 16:22:41 -0400

    initial import of App::sdb_client

---
+++ lib/App/sdb_client.pm
@@ -0,0 +1,40 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package App::sdb_client;
+# VERSION
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+# ABSTRACT: No abstract given for App::sdb_client
+
+=for Pod::Coverage method_names_here
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use App::sdb_client;
+
+= DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+= USAGE
+
+Good luck!
+
+= SEE ALSO
+
+Maybe other modules do related things.
+
+=end wikidoc
+
+=cut
+
+# vim: ts=2 sts=2 sw=2 et:
